212ed1c24c7ebc1b1fd6d6398271cab30d77b5a5
kan-bayashi <hayashi.tomoki@g.sp.m.is.nagoya-u.ac.jp>
2020-03-26 08:35:20 +0900

    added Jellybeans_like theme

---
+++ themes/Jellybeans_like.js
@@ -0,0 +1,4 @@
+t.prefs_.set('color-palette-overrides',["#333333", "#cb484c", "#75a24d", "#fd9f4d", "#446ec9", "#cc98de", "#6f9cc2", "#ffffff", "#535353", "#ef524f", "#8bc249", "#ffd33a", "#64b5f6", "#e9b1e7", "#89c7ce", "#ffffff"]);
+t.prefs_.set('foreground-color', "#eaeaea");
+t.prefs_.set('background-color', "#131313");
+t.prefs_.set('cursor-color', 'rgba(255,165,96,0.5)');
